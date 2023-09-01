b0bce1187b5d5ddc8ee4b94cc34cad64de339452
Caden.Finsinyur <caden.finsinyur@gmail.com>
2023-09-01 11:26:23 +0800

    Updated notebook and .py file

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,7 @@
 # Import WRDS library
 import wrds
 import pandas as pd
+import os
 
 def read_sql_script(fname, db, date_cols = ['trading_date']):
     fd = open(fname, 'r')
@@ -12,27 +13,30 @@ def read_sql_script(fname, db, date_cols = ['trading_date']):
     return df
 
 def main():
+    # get current path
+    this_path = os.path.abspath(os.curdir)
+    print(this_path)
     # Establish live connection; requires user login (passwords will be masked)
     db = wrds.Connection()
 
     # Get VIX
     # library = 'cboe_all', table = 'cboe' 
-    vix_fn = "./SQL/get_vix.sql"
-    df_vix = read_sql_script(vix_fn, db)
+    vix_fn = "get_vix.sql"
+    df_vix = read_sql_script(os.path.join(this_path,'SQL', vix_fn), db)
     df_vix.to_csv('./data/vix_20130301_20230228.csv', index = False)
     print('VIX data extracted')
 
     # Get S&P 500 Index
     # S&P 500 index price:  library='optionm_all', table='secprd'
-    spx_fn = "./SQL/get_spx.sql"
-    df_spx = read_sql_script(spx_fn, db)
+    spx_fn = "get_spx.sql"
+    df_spx = read_sql_script(os.path.join(this_path,'SQL', spx_fn), db)
     df_spx.to_csv('./data/spx_20130301_20230228.csv', index = False)
     print('SPX data extracted')
 
     # Get S&P 500 Call, Put options volume and flag
     # volume: library = optionm_all, table = opvold
-    opvol_fn = "./SQL/get_option_volume.sql"
-    df_opvol = read_sql_script(opvol_fn, db)
+    opvol_fn = "get_option_volume.sql"
+    df_opvol = read_sql_script(os.path.join(this_path,'SQL', opvol_fn), db)
     df_opvol.to_csv('./data/opvol_20130301_20230228.csv', index = False)
     print('option volume data extracted')
 
